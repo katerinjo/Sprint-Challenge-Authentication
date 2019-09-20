1a03837f8b17da1d1e275f1d650c454bfda0a3ee
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-20 08:57:21 -0700

    authentication middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,24 @@
   before granting access to the next middleware/route handler
 */
 
+const db = require('../database/userData');
+const bcrypt = require('bcrypt');
+
 module.exports = (req, res, next) => {
+  if (req.session && req.session.user) {
+    const { username, password } = req.session.user;
+    db.readUser(username)
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'invalid credentials' });
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  }
+
   res.status(401).json({ you: 'shall not pass!' });
 };
