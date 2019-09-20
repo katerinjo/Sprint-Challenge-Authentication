d8c768843fd48429ecfba7ccd36b30e9bc20964b
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-20 09:32:10 -0700

    login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,6 +21,18 @@ router.post('/login', authenticate, (req, res) => {
   // implement login
   const { username, password } = req.body;
 
+  db.read(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+        res.status(200).json({ message: 'logged in'});
+      } else {
+        res.status(401).json({ message: 'invalid credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
